7c821f5a22ed404e3578ca7ba88c99bbccd96d71
Veljko Petrovic <pveljko@gmail.com>
2020-12-22 14:55:57 +0100

    Mlflow adaptation complete. Prediction now works as intended and is accelerated through multiprocess execution and async mlflows.

--- codes/inference.py
+++ codes/inference.py
@@ -3,24 +3,24 @@ from keras.models import model_from_json
 import os
 import utils
 import numpy as np
+from pathlib import Path
 
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--fundus_dir', type=str, default="../data/{}/test/images/")
-parser.add_argument('--mask_dir', type=str, default="../data/{}/test/mask/")
-parser.add_argument('--out_dir', type=str, default="../inference_outputs/{}")
+parser.add_argument('--out_dir', type=str, default="../results/{}")
 parser.add_argument('--f_model_dir', type=str, default="../pretrained")
 parser.add_argument('--f_model', type=str, default="../pretrained/{}_best.json")
 parser.add_argument('--f_weights', type=str, default="../pretrained/{}_best.h5")
 parser.add_argument('--f_dataset', type=str, default="DRIVE")
+parser.add_argument('--spec', type=str, default="1.tmp")
+
 
 args = parser.parse_args()
 
-fundus_dir = args.fundus_dir
-mask_dir = args.mask_dir
 out_dir = args.out_dir
 dataset = args.f_dataset
+spec = args.spec
 
 if args.f_model is not None:
     f_model = args.f_model
@@ -55,17 +55,27 @@ elif dataset=="STARE":
 elif dataset=="miniDROPS":
     ori_shape=(1,480,640)
 
-fundus_files=utils.all_files_under(fundus_dir.format(dataset))
-mask_files=utils.all_files_under(mask_dir.format(dataset))
+fundus_files = []
+mask_files = []
+with open(spec, "r") as sp:
+    l = sp.readlines()
+    fundus_files = [v.strip() for (i, v) in enumerate(l) if i % 2 == 0]
+    mask_files = [v.strip() for (i, v) in enumerate(l) if i % 2 == 1]
+
+#fundus_files=utils.all_files_under(fundus_dir.format(dataset))
+#mask_files=utils.all_files_under(mask_dir.format(dataset), extension="png")
+
+
 for index,fundus_file in enumerate(fundus_files):
-    print("processing {}...".format(fundus_file))
-    # load imgs
+    print(f'Processing {fundus_file}')
     img=utils.imagefiles2arrs([fundus_file])
     mask=utils.imagefiles2arrs([mask_files[index]])
-    
+    dx = 1 if img.shape[1] > ori_shape[1] else 0
+    dy = 1 if img.shape[2] > ori_shape[2] else 0
+    img = np.array([img[0,dx:,dy:,:]])
     # z score with mean, std (batchsize=1)
-    mean=np.mean(img[0,...][mask[0,...] == 255.0],axis=0)
-    std=np.std(img[0,...][mask[0,...] == 255.0],axis=0)
+    mean=np.mean(img[0,...][mask[0,...] > 0.99],axis=0)
+    std=np.std(img[0,...][mask[0,...] > 0.99],axis=0)
     img[0,...]=(img[0,...]-mean)/std
     
     # run inference
@@ -73,4 +83,6 @@ for index,fundus_file in enumerate(fundus_files):
     vessel_img=model.predict(padded_img,batch_size=1)*255
     cropped_vessel=utils.crop_to_original(vessel_img[...,0], ori_shape)
     final_result=utils.remain_in_mask(cropped_vessel[0,...], mask[0,...])
-    Image.fromarray(final_result.astype(np.uint8)).save(os.path.join(out_dir.format(dataset),os.path.basename(fundus_file)))
+    outfile = Path(out_dir.format(dataset))
+    outfile = outfile / f'{Path(fundus_file).stem}.png'
+    Image.fromarray(final_result.astype(np.uint8)).save(outfile)
---
+++ codes/predict_workflow.py
@@ -0,0 +1,47 @@
+import glob
+import mlflow
+import os
+import utils
+
+from pathlib import Path
+
+project_path = Path('.').resolve()
+
+def predict_parallel():
+    root = Path('..')
+    threads = 8
+    for d in ['DRIVE', 'STARE']:
+        allimgs = utils.all_files_under(root / "data" / "eval" / d / "images")
+        allmasks = utils.all_files_under(root / "data" / "eval" / d / "masks", extension="png")
+        jobs = []
+        assert(len(allimgs) == len(allmasks))
+        n = len(allimgs)
+        s = n // threads
+        for i in range(threads):
+            lower = i * s
+            upper = min(s*(i+1), n)
+            img = allimgs[lower:upper]
+            masks = allmasks[lower:upper]
+            with open(f'{i}.tmp','w') as f:
+                for j in range(len(img)):
+                    f.write(f'{img[j]}\n{masks[j]}\n')
+            p = {
+                    'dataset' : d,
+                    'spec' : f'{i}.tmp' 
+            }
+            print(f"Starting job {i}")
+            jobs.append(
+                mlflow.projects.run(
+                    uri=str(project_path),
+                    entry_point='test',
+                    parameters=p,
+                    experiment_name=f'VGAN-{i}',
+                    use_conda=False,
+                    synchronous=False
+                )
+            )
+        for j in jobs:
+            j.wait()
+
+if __name__ == '__main__':
+    predict_parallel()   
